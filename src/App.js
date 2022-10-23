36fc1b4d73120c48b7a95c7161dfa3e3a731f47e
David Daniels <ddaniels7009@gmail.com>
2022-10-23 18:22:50 -0400

    added initial functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import './App.css';
+import { Component } from "react";
+import Name from './components/name'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      user: {
+        name: 'edi',
+        email: 'edi@gmail.com'
+      },
+    };
+  }
+
+  render() {
+
+    return (
+      <div>
+        <form>
+          <label>Name</label>
+          <input type="test"></input>
+
+          <label>email</label>
+          <input type="test"></input>
+
+          <button>Submit info</button>
+        </form>
+
+        <Name user={this.state.user} />
+
+      </div>
+
+    );
+  }
 }
 
 export default App;
---
+++ src/components/name.js
@@ -0,0 +1,23 @@
+import { Component } from "react";
+
+class Name extends Component {
+
+    constructor(props) {
+        super(props);
+        console.log(this.props)
+
+    }
+
+    render(){
+        
+        return (
+            <div>  
+                <h1>{this.props.user.name}</h1>
+                <h2>{this.props.user.email}</h2>
+            </div>
+        );
+    }
+
+}
+
+export default Name
